c0fb929f1542ffeb4ce0541896b24c48b5149c07
kevin <mattisonii@yahoo.com>
2016-03-29 13:54:18 -0700

    Create news and portland_rinks hbs files.

--- app/router.js
+++ app/router.js
@@ -5,8 +5,13 @@ const Router = Ember.Router.extend({
   location: config.locationType
 });
 
-Router.map(function() { //This is a route. It will load the NEWS route handler
-  this.route('news');   // ..app/routes/news.js
+//This is a route. It will load the NEWS route handler
+// ..app/routes/news.js
+Router.map(function() {
+  this.route('news');
+  this.route('portland_rinks');
 });
 
 export default Router;
+
+//URL localhost:4200 ==> ROUTER (router.js) ==> News Route (routes/news.js) ==> News Template (templates/news.hbs)
